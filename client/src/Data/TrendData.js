97b52ee35170daca63bce2bba560008981d14d3a
anayet34 <anayetsarker40@gmail.com>
2022-12-13 10:29:21 +0600

    first commit

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "Anayet",
+    shares: 97,
+  },
+  {
+    name: "Rahman",
+    shares: 80.5,
+  },
+  {
+    name: "Sarker",
+    shares: 75.5,
+  },
+  {
+    name: "Mahabub",
+    shares: 72,
+  },
+  {
+    name: "Junaiet",
+    shares: 71.9,
+  },
+  {
+    name: "Farabi",
+    shares: 20,
+  },
+];
